14baa3c84142bac0dc81eb8ddb3d7d4e56a32366
James Moreau <jmorea03@uoguelph.ca>
2019-09-13 00:02:31 -0400

    started ds_memory source file

--- src/ds_memory.c
+++ src/ds_memory.c
@@ -1,2 +1,25 @@
+#include "ds_memory.h"
+
 struct ds_file_struct ds_file;
 struct ds_counts_struct ds_counts;
+
+int ds_create(char *filename, long size) {
+    int i;
+
+    struct ds_file_struct myFile;
+    myFile.fp = fopen(filename, "wb+");
+    if (myFile.fp == NULL) { return -1;}
+
+    myFile.block[0].start = 0;
+    myFile.block[0].length = size;
+    myFile.block[0].alloced = 0;
+
+    for(i=0; i< MAX_BLOCKS; i++) {
+        myFile.block[i].start = 0;
+        myFile.block[i].length = 0;
+        myFile.block[i].alloced = 0;
+    }
+
+    fclose(myFile.fp);
+    return 0;
+}
