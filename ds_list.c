7980c4791cf37b4d966a10521be3a617bce4b6b6
James Moreau <jmorea03@uoguelph.ca>
2019-09-28 13:44:25 -0400

    ds insert done

--- ds_list.c
+++ ds_list.c
@@ -22,19 +22,31 @@ int ds_init_list() {
 ds_insert(int value, long index) {
     int i;
     long pos;
-    long myPointer;
-    long previous_loc;
     long last_item_loc;
     struct ds_list_item_struct previous, new;
     
     pos = 0;
-    previous_loc = 0;
+    last_item_loc = 0;
     ds_read(&previous.next, pos, sizeof(previous.next));
 
-    for (i = index; i>=0; i--) {
-        if (previous.next) { return -1;}
-
-        
+    for (i = index; i>0; i--) {
+        if (previous.next == -1) { return -1;}
+        last_item_loc = previous.next;
+        ds_read(&previous, previous.next, sizeof(struct ds_list_item_struct));     
     }
 
+    new.item = value;
+    new.next = previous.next;
+    previous.next = ds_malloc(sizeof(struct ds_list_item_struct));
+    if (previous.next == -1) { return 1;}
+
+    if(ds_write(previous.next, &new, sizeof(struct ds_list_item_struct))) {return 1;}
+
+    if(last_item_loc ==  0) { 
+        return 0;
+    } else {
+        ds_write(last_item_loc, &previous, sizeof(struct ds_list_item_struct));
+    }
+    
+    return 0;
 }
