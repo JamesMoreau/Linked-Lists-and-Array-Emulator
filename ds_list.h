b310e0b2c97e9f3fcb3591a1a5ce2a2e978aad2a
TheseusQQ <jamiemoreau@live.com>
2019-09-27 23:50:31 -0400

    added linked list base files

---
+++ ds_list.h
@@ -0,0 +1,22 @@
+/*guards*/
+#ifndef __LISTH__
+#define __LISTH__
+
+/*Libraries*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*Globals*/
+
+
+/*Structures*/
+struct ds_list_item_struct {
+    int item;
+    long next;
+};
+
+/*Function*/
+
+
+#endif
